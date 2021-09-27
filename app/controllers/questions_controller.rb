bd83af09f781e5a5a7241711ffe7d3626241075c
dajhely angles <assiaxramx10@gmail.com>
2021-09-27 12:18:08 -0500

    implementing routing and displaying questions and answers in view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,22 @@
 class QuestionsController < ApplicationController
-def ask
-end
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    # @answer = ''
+    # @answer = begin
+    #   case @question
+    #   when "I am going to work" then "Great!"
+    #   when @question.ends_with?('?') then "Silly question, get dressed and go to work!"
+    #   else "I don't care"
+    #   end
+    # end
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
